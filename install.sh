f96769a8b6a3169610c7de0a40e8d7c57e5ee20a
RangerUFO <ufownl@gmail.com>
2020-03-04 15:30:14 +0800

    Improve the installation script

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
-#! /bin/bash
+#! /bin/bash -x
 
+SOURCE_DIR=`pwd`
 # install pathogen
-mkdir -p ~/.vim/autoload && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
+mkdir -p ~/.vim/autoload && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
 # install vimrc
-ln -s `pwd`/bundle ~/.vim/bundle
-ln -s `pwd`/vimrc ~/.vimrc
+ln -s $SOURCE_DIR/bundle ~/.vim && ln -s $SOURCE_DIR/vimrc ~/.vimrc
